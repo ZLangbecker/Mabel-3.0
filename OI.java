daf04259add79c8b359ed84faa781f0ceff47419
Zoe Langbecker <FRCZZLangbecc@gmail.com>
2018-08-16 20:16:34 -0700

    Copied some commands and understood them
    yieks

--- OI.java
+++ OI.java
@@ -7,6 +7,8 @@
 
 package org.usfirst.frc.team4043.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -39,4 +41,15 @@ public class OI {
 	// Start the command when the button is released and let it run the command
 	// until it is finished as determined by it's isFinished method.
 	// button.whenReleased(new ExampleCommand());
+		
+	public Joystick driveStick = new Joystick(5);
+	public static Joystick coStick = new Joystick(1);
+
+	public Joystick getDriveStick() {
+		return driveStick;
+	}
+	public static Joystick getCoStick() {
+		return coStick;
+	}
+
 }
